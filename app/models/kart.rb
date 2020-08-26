992682c48b62aca578a6888e8d33c5f9be2e581c
Haloren <mckennakyler@gmail.com>
2020-08-26 00:44:21 -0700

    added model relationships & seeded database

--- app/models/kart.rb
+++ app/models/kart.rb
@@ -1,4 +1,8 @@
 class Kart < ApplicationRecord
   belongs_to :club
   belongs_to :user
+
+  has_many :reviews
+  has_many :users, through: :reviews
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@
 User.create(username: "Miss Fuentes", email: "email@email.com", password: "password")
 User.create(username: "Bunta", email: "email1@email1.com", password: "password")
 
-Garage.create(name: "Tej's Garage")
-Garage.create(name: "Takahashi Garage")
+Club.create(name: "Tej's Club")
+Club.create(name: "Night Kids")
 
-Kart.create(name: "Lightning", driver: "Knome", body: "Magnum", wheels: "White Star", garage_id: Garage.first.id, user_id: User.first.id)
-Kart.create(name: "Magneto", driver: "Trooper", body: "Cytruck", wheels: "Solid", garage_id: Garage.first.id, user_id: User.second.id)
-Kart.create(name: "Hachi-Go", driver: "Racer", body: "Panda", wheels: "Steele", garage_id: Garage.second.id, user_id: User.second.id)
+Kart.create(name: "Lightning", driver: "Knome", body: "Magnum", wheels: "White Star", club_id: Club.first.id, user_id: User.first.id)
+Kart.create(name: "Magneto", driver: "Trooper", body: "Cytruck", wheels: "Solid", club_id: Club.first.id, user_id: User.second.id)
+Kart.create(name: "Hachi-Go", driver: "Racer", body: "Panda", wheels: "Steele", club_id: Club.second.id, user_id: User.second.id)
 
 Review.create(rating: 1, content: "Content 1, Review written here", user_id: User.first.id, kart_id: Kart.first.id)
 Review.create(rating: 2, content: "Content 2, Review written here", user_id: User.second.id, kart_id: Kart.second.id)
