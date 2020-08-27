fae7bbda3a6b9f9f83a0cf489882ecd999973af6
Haloren <mckennakyler@gmail.com>
2020-08-26 19:36:10 -0700

    added Sessions destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
 
     def new
-
     end
 
     def create
 
     end
     
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
 end
