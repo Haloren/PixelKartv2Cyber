6b379e67ee935ac9e515448f6d8bd1b610362e12
Haloren <mckennakyler@gmail.com>
2020-08-29 22:08:10 -0700

    changed Reviews index to display data based on @reviews = Review.all

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,8 +2,7 @@ class ReviewsController < ApplicationController
     before_action :set_user, only: [:index]
     
     def index
-        @karts = Kart.all
-        
+        @reviews = Review.all
     end
 
 end
