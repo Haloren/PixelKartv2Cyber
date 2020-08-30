988015b1cc82022d8c4cde66ddab369c6d86e956
Haloren <mckennakyler@gmail.com>
2020-08-29 17:16:20 -0700

    started clubs index page

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -2,9 +2,17 @@ class ClubsController < ApplicationController
     before_action :set_user, only: [:index, :show]
 
     def index
+        @clubs = Club.all
+        byebug
+        if @clubuser = User.find_by(id: params[:user_id])
+            @clubs = @clubuser.clubs
+        else 
+            "You currently are not a member of any clubs."
+        end
     end
 
     def show
+        @club = Club.find_by(id: params[:id])
     end
 
 end
