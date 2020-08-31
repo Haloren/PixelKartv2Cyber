082fc1346ef30478fd3cb4d8c55142d61283e9d2
Haloren <mckennakyler@gmail.com>
2020-08-31 15:51:54 -0700

    fixed review validation and changed grid for clubs

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :kart
 
-  validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 4 }
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 5 }
   validates :content, length: {maximum: 150}
   validates :kart, uniqueness: { scope: :user } # only one review should be allowed by a user
 
